5780c5640498989c3de14a95ec40ef783e1eb263
Jannatun Nayeem <jannat.nayeem01@gmail.com>
2023-10-17 00:49:25 +0700

    Reactive Login Form Completed

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl,FormGroup } from '@angular/forms';
+import { Validators,ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent {
+  submitted = false;
 
+  loginForm = new FormGroup(
+    {
+      email: new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      password: new FormControl('',[Validators.required]),
+      rememberMe: new FormControl('')
+    })
+    onSubmit(){
+      this.submitted = true;
+    }
+  
+  
 }
